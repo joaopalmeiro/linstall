db31e0652d358fd6b73d50a049c65ec393d943ae
João Palmeiro <joaommpalmeiro@gmail.com>
2022-08-05 15:37:59 +0100

    Add flakeheaven

--- linstall/console.py
+++ linstall/console.py
@@ -2,5 +2,9 @@ import click
 import pyperclip
 
 
+# https://click.palletsprojects.com/en/8.1.x/api/#click.version_option
+@click.command()
+@click.version_option()
 def run():
-    print("Hello, World!")
+    """Generate a list of installation commands for a package to add to README files."""
+    click.echo("Hello, World!")
